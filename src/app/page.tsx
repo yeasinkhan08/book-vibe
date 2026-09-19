53886922faa5924934c7a2d718cdc77aa9a42570
yeasinkhan08 <kyeasin58@gmail.com>
2026-09-19 23:11:52 +0600

    feat:add a banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Banner from "@/components/homepage/Banner";
 import React from "react";
 
 const homePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default homePage;
--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -3,15 +3,20 @@ import Image from "next/image";
 import bannerImg from "@/assets/hero_img.jpg";
 const Banner = () => {
   return (
-    <div className="grid grid-cols-2 gap-4 items-center">
-      <div>
-        <h2>Books to freshen up your bookshelf </h2>
-        <button>view the task</button>
+    <section className="py-20">
+      <div className="container mx-auto grid grid-cols-2 gap-4 items-center bg-amber-50 rounded-3xl p-4 ">
+        <div className="space-y-4">
+          <h2 className="font-bold text-5xl">
+            Books to freshen up <br /> your bookshelf{" "}
+          </h2>
+          <button className="btn btn-success">view the task</button>
+        </div>
+        <div>
+          <Image src={bannerImg} alt="" />
+        </div>
       </div>
-      <div>
-        <Image src={bannerImg} alt="" />
-      </div>
-    </div>
+      2
+    </section>
   );
 };
 
